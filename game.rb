a6bb8e9853576ef577e582e92d31e8e4abab11c9
Dominic Tremblay <dominictremblay24@gmail.com>
2018-11-04 23:11:04 -0500

    created summary

--- game.rb
+++ game.rb
@@ -3,17 +3,27 @@ require_relative './jedi'
 class Game
 
   attr_reader :jedis
+  attr_accessor :round
 
   def initialize
     jedi1 = Jedi.new('Yoda')
     jedi2 = Jedi.new('vador')
     @jedis=[jedi1, jedi2]
+    @round = 1
   end
 
   def run
-    puts @jedis[0].inspect
-    puts @jedis[1].inspect
+    
+    puts "====== Round ##{@round} ======"
+    puts "\n"
+    summary
+    puts "----- Attacking -----"
   end
-
+    
+    def summary
+      puts "------ Summary ------"
+      puts @jedis.map{|jedi| jedi.summary}.join("\n")
+      puts "\n"
+    end
 
 end
--- jedi.rb
+++ jedi.rb
@@ -1,8 +1,15 @@
 class Jedi
+
+  attr_reader :name, :hp, :ap
+
   def initialize(name)
     @name=name
     @hp=20
     @ap=10
+  end
+
+  def summary
+    "#{@name} \t - HP: #{@hp} AP: #{@ap}"
   end
 
 end
