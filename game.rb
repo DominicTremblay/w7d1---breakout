662bbfccb8e9fd6555b8c4828f239d68c744621e
Dominic Tremblay <dominictremblay24@gmail.com>
2018-11-04 23:01:01 -0500

    inspecting the 2 jedis variables

--- game.rb
+++ game.rb
@@ -1,10 +1,19 @@
+require_relative './jedi'
+
 class Game
+
+  attr_reader :jedis
 
   def initialize
     jedi1 = Jedi.new('Yoda')
     jedi2 = Jedi.new('vador')
+    @jedis=[jedi1, jedi2]
   end
 
-  
+  def run
+    puts @jedis[0].inspect
+    puts @jedis[1].inspect
+  end
+
 
 end
